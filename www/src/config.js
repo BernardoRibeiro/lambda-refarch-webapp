2979f699c3bdca135af4f42e0da46c462174cbd6
BernardoRibeiro <bernardoribeiro@tecnico.ulisboa.pt>
2024-04-17 20:42:31 +0100

    add config

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,31 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "None",     // CognitoClientID
-  "api_base_url": "None",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "None",                   // CognitoDomainName
-  "redirect_url": "None"                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "2l3cc2ora7di5ucdc20gmhimfn",     // CognitoClientID
+  "api_base_url": "https://w7dbi02czd.execute-api.eu-north-1.amazonaws.com/{StageNameParam}",                                     // TodoFunctionApi
+  "cognito_hosted_domain": "mytodoappdemo2-lambda-refarch-webapp.auth.eu-north-1.amazoncognito.com",                   // CognitoDomainName
+  "redirect_url": "https://master.d1lfzx10m4cy0l.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
+
+
+// Key                 CognitoClientID                                                                                                                                                                 
+// Description         The Cognito UserPool Client ID                                                                                                                                                  
+// Value               2l3cc2ora7di5ucdc20gmhimfn                                                                                                                                                      
+
+// Key                 CognitoDomainName                                                                                                                                                               
+// Description         The Cognito Hosted UI Domain Name                                                                                                                                               
+// Value               mytodoappdemo2-lambda-refarch-webapp.auth.eu-north-1.amazoncognito.com                                                                                                          
+
+// Key                 AmplifyURL                                                                                                                                                                      
+// Description         -                                                                                                                                                                               
+// Value               https://master.d1lfzx10m4cy0l.amplifyapp.com                                                                                                                                    
+
+// Key                 CognitoID                                                                                                                                                                       
+// Description         The Cognito UserPool ID                                                                                                                                                         
+// Value               eu-north-1_lPAT0DGuX                                                                                                                                                            
+
+// Key                 TodoFunctionApi                                                                                                                                                                 
+// Description         API Gateway endpoint URL for Prod stage                                                                                                                                         
+// Value               https://w7dbi02czd.execute-api.eu-north-1.amazonaws.com/{StageNameParam}  
